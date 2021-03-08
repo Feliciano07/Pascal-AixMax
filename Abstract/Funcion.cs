55ab750d03d9b9b3962626604d6ddd000dc403e7
Feliciano07 <fernandoxhajon122@gmail.com>
2021-03-08 16:49:59 -0600

    Se agregaron optimizacines, pero al chile no funciona

--- Abstract/Funcion.cs
+++ Abstract/Funcion.cs
@@ -11,27 +11,20 @@ namespace Pascal_AirMax.Abstract
     {
 
         private LinkedList<Parametro> parametros;//TODO: cambiar en un futuro a tipo parametro
-        private LinkedList<Objeto> actuales;
+
         private string nombre_fun;
 
         public Funcion(int linea, int columna, LinkedList<Parametro> parametros, string nombre): base(linea, columna)
         {
             this.parametros = parametros;
             this.nombre_fun = nombre;
-            this.actuales = new LinkedList<Objeto>();
         }
-
 
         //obtener parametros de la funcion
         public LinkedList<Parametro> getParametros()
         {
             return this.parametros;
 
-        }
-
-        public LinkedList<Objeto> getActuales()
-        {
-            return this.actuales;
         }
 
         public string getNombre()
